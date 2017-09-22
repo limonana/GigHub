7d92006f69545d00226416c491434c80a15da71f
Shani Fedida <shani.fedida@gmail.com>
2017-09-22 14:06:57 +0300

    create basic html shwing gigs

--- GigHub/src/app/upcoming-gigs/upcoming-gigs.component.ts
+++ GigHub/src/app/upcoming-gigs/upcoming-gigs.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Gig } from '../model/Gig';
+const MockGigs: Gig[] = [
+  { Name:"Hello World", Artist:"Developer", Location:"LAB", Time:"TODAY 6:00 PM",Genre:"",Description:"" },
+  { Name:"We LOVE TS", Artist:"Taylor Swift", Location:"US", Time:"TODAY 6:00 PM",Genre:"",Description:"" }  
+];
 
 @Component({
   selector: 'app-upcoming-gigs',
   templateUrl: './upcoming-gigs.component.html',
   styleUrls: ['./upcoming-gigs.component.css']
 })
-export class UpcomingGigsComponent implements OnInit {
 
-  constructor() { }
+
+export class UpcomingGigsComponent implements OnInit {
+  gigs=MockGigs;
+  constructor() {          
+  }
 
   ngOnInit() {
   }
