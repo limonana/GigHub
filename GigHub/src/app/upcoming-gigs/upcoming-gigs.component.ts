76b16711ed1fd951d0603cfb9178031610778185
Shani Fedida <shani.fedida@gmail.com>
2017-10-14 01:02:45 +0300

    fix ImageUrl

--- GigHub/src/app/upcoming-gigs/upcoming-gigs.component.ts
+++ GigHub/src/app/upcoming-gigs/upcoming-gigs.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Gig } from '../model/Gig';
 const MockGigs: Gig[] = [
   { Artist: 'developer', Location: 'BGU LAB', Time: 'TODAY 6:00 PM', Genre: '', Description: 'dasasdas',
-  ImageURL: 'aa' },
+  ImageURL: '' },
   { Artist: 'taylor swift', Location: 'US Califronia bla bla Hall', Time: 'TODAY 6:00 PM', Genre: '', 
   Description: 'fdsfdsfds', ImageURL: 'https://i.scdn.co/image/54969dd8c24693d05c8445c0de4ad74a719f1d65' },
   { Artist: 'taylor swift', Location: 'US Califronia bla bla Hall', Time: 'TODAY 6:00 PM', Genre: '', 
