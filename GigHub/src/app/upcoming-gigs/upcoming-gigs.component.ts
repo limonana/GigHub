17865c83b17e86ec883da6a1dbbe6a54e064f1ab
Shani Fedida <shani.fedida@gmail.com>
2017-10-13 15:09:10 +0300

    convert divs to p remove empty css styles

--- GigHub/src/app/upcoming-gigs/upcoming-gigs.component.ts
+++ GigHub/src/app/upcoming-gigs/upcoming-gigs.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Gig } from '../model/Gig';
 const MockGigs: Gig[] = [
-  { Artist: 'developer', Location: 'LAB', Time: 'TODAY 6:00 PM', Genre: '', Description: '' },
-  { Artist: 'taylor swift', Location: 'US', Time: 'TODAY 6:00 PM', Genre: '', Description: '' }
+  { Artist: 'developer', Location: 'BGU LAB', Time: 'TODAY 6:00 PM', Genre: '', Description: '' },
+  { Artist: 'taylor swift', Location: 'US Califronia bla bla Hall', Time: 'TODAY 6:00 PM', Genre: '', Description: '' }
 ];
 
 @Component({
