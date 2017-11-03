d12753d785cce7112f75d480901478af640fe913
Shani Fedida <shani.fedida@gmail.com>
2017-11-03 14:45:41 +0200

    #20 move navigation bar to its own componenet

--- GigHub/src/app/app.module.ts
+++ GigHub/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { UpcomingGigsComponent } from './upcoming-gigs/upcoming-gigs.component';
 import { RouterModule, Routes } from '@angular/router';
 import { GigCardComponent } from './upcoming-gigs/gig-card/gig-card.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UpcomingGigsComponent,
-    GigCardComponent
-  ],
+    GigCardComponent,
+    NavigationBarComponent
+],
   imports: [
     BrowserModule,
     RouterModule.forRoot([{ path: 'upcoming-gigs', component: UpcomingGigsComponent }])
